e43a7ed7281c594986fae57138411811eb0ae525
jkaeshim <Jak3shim@gmail.com>
2023-10-17 10:34:24 -0700

    .

--- _notebooks/2023-10-17-Postman_Test.py
+++ _notebooks/2023-10-17-Postman_Test.py
@@ -1,17 +1,37 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-data = []  # This will store our data
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify(data)
-
-@app.route('/api/data', methods=['POST'])
-def add_data():
-    new_data = request.get_json()
-    data.append(new_data)
-    return jsonify({"message": "Data added successfully"}), 201
-
-if __name__ == '__main__':
-    app.run(debug=True)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask, request, jsonify\n",
+    "\n",
+    "app = Flask(__name__)\n",
+    "data = []  # This will store our data\n",
+    "\n",
+    "@app.route('/api/data', methods=['GET'])\n",
+    "def get_data():\n",
+    "    return jsonify(data)\n",
+    "\n",
+    "@app.route('/api/data', methods=['POST'])\n",
+    "def add_data():\n",
+    "    new_data = request.get_json()\n",
+    "    data.append(new_data)\n",
+    "    return jsonify({\"message\": \"Data added successfully\"}), 201\n",
+    "\n",
+    "if __name__ == '__main__':\n",
+    "    app.run(debug=True)\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
