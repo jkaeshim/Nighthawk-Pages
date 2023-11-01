5415f1814690aaf7d710a64f5d9a400b4dd10dec
DanteA-tech <dante.atanassov@yahoo.com>
2023-11-01 13:47:31 -0700

    backend connection

--- templates/movie.py
+++ templates/movie.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(__name)
 
-@app.route('/')
-def index():
-    # Make an HTTP GET request to the TMDb API
-    api_key = 'bd74380ad0f3a6bc2db537543036493a'  # Replace with your TMDb API key
-    url = f'https://api.themoviedb.org/3/movie/550?api_key={api_key}'
+# SQLite database setup and API endpoints
+
+@app.route('/api/movies', methods=['GET', 'POST', 'DELETE'])
+def manage_movies():
+    if request.method == 'GET':
+        # Handle GET request to fetch and return favorite movies
+        # You can query the SQLite database and return the data as JSON
+
+    elif request.method == 'POST':
+        # Handle POST request to add a favorite movie to the database
+        # Extract movie details from the request and insert them into the database
+
+    elif request.method == 'DELETE':
+        # Handle DELETE request to remove a favorite movie from the database
+        # Extract the movie ID from the request and delete the record
+
+@app.route('/api/tmdb_movie', methods=['GET'])
+def fetch_tmdb_movie():
+    # Handle GET request to fetch movie data from The Movie Database (TMDb) API
+    tmdb_api_url = 'https://api.themoviedb.org/3/movie/550?api_key=bd74380ad0f3a6bc2db537543036493a'
 
     try:
-        response = requests.get(url)
+        response = requests.get(tmdb_api_url)
         data = response.json()
-        movies = data.get('movies', [])  # Assuming the movie data is in a 'movies' key
+        return jsonify(data)
     except requests.exceptions.RequestException as e:
-        print('Error fetching movie data:', e)
-        movies = []
-
-    return render_template('index.html', movies=movies)
+        return jsonify({'error': 'Error fetching movie data from TMDb API'})
 
 if __name__ == '__main__':
     app.run(debug=True)
